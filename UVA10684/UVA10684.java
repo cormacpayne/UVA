9d518712ff947be80699f5a70ef9bd87af19754f
cormacpayne <cormacpayne@ufl.edu>
2014-10-02 19:03:43 -0400

    Create UVA10684.java

---
+++ UVA10684/UVA10684.java
@@ -0,0 +1,32 @@
+import java.util.*;
+/*
+ *	UVa 10684 - The jackpot
+ *  Algorithm - Max 1D Range Sum
+ */
+
+class UVA10684 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+
+		while (n != 0) {
+			int sum = 0;
+			int ans = 0;`
+
+			for (int i = 0; i < n; i++) {
+				int num = in.nextInt();
+				sum += num;
+				ans = Math.max(ans, sum);
+				if (sum < 0) sum = 0;
+			}
+
+			if (ans > 0) {
+				System.out.println("The maximum winning streak is " + ans + ".");
+			} else {
+				System.out.println("Losing streak.");
+			}
+
+			n = in.nextInt();
+		}
+	}
+}
